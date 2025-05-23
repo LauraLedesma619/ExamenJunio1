82608ed9798852561856eaa34bb56ff60d8ea757
LauraLedesma619 <148544156+LauraLedesma619@users.noreply.github.com>
2025-05-23 16:48:42 +0200

    Add files via upload

---
+++ src/Laura.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Laura {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+			
+			
+			System.out.print("Introduce tu nombre");
+			String nombre = sc.nextLine();
+			
+			System.out.println("Hola " + nombre + ", perteneces a la generación del futuro");
+
+	}
+
+}
